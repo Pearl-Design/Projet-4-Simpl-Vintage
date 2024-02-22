f6d26cbcc39b54d97f447ed1a4a82538c57dc1fa
Pearl Design <pearldesign06@gmail.com>
2024-02-23 00:03:46 +0100

    Installation d'un template et création de la page d'accueil

--- wp-content/themes/simpl-vintage/functions.php
+++ wp-content/themes/simpl-vintage/functions.php
@@ -1,9 +1,13 @@
 <?php
-// ajout css
-add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 
-function theme_enqueue_styles() {
-	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+function oceanwp_child_enqueue_parent_style() {
+	// Dynamically get version number of the parent stylesheet (lets browsers re-cache your stylesheet when you update your theme)
+	$theme   = wp_get_theme( 'OceanWP' );
+	$version = $theme->get( 'Version' );
+	// Load the stylesheet
+	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css?t='.time(), array( 'oceanwp-style' ), $version );
+
 }
+add_action( 'wp_enqueue_scripts', 'oceanwp_child_enqueue_parent_style' );
 
 ?>
